29644ff5a2e9b4fdaf05ba596188ce5cbf17f24e
Vlad <427departament@gmail.com>
2025-12-12 13:33:19 +0300

    edit permitions

--- klippy/extras/adc_temperature.py
+++ klippy/extras/adc_temperature.py
@@ -66,7 +66,7 @@ class HelperTemperatureDiagnostics:
         try:
             last_temp = self.calc_temp_cb(last_value)
             tempstr = "%.3f" % (last_temp,)
-            subprocess.run(["/home/pi/klipper/scripts/off_psu.sh"])
+            subprocess.run(["sudo", "/home/pi/klipper/scripts/off_psu.sh"])
         except e:
             logging.exception("Error in calc_temp callback")
         return ("Sensor '%s' temperature %s not in range %.3f:%.3f"
