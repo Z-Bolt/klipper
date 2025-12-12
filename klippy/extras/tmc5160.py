eefff3e9bd6b0bffdd26b5e0d1c6a1900bc3a1d5
Vlad <427departament@gmail.com>
2025-12-12 14:52:07 +0300

    Feature: - убран GSTAT - добавлено отключение PSU при ошибках по температуре - добавлена фукнция компенсации искривления балки

--- klippy/extras/tmc5160.py
+++ klippy/extras/tmc5160.py
@@ -10,7 +10,6 @@ TMC_FREQUENCY=12000000.
 
 Registers = {
     "GCONF":            0x00,
-    "GSTAT":            0x01,
     "IFCNT":            0x02,
     "SLAVECONF":        0x03,
     "IOIN":             0x04,
@@ -73,7 +72,7 @@ Registers = {
 }
 
 ReadRegisters = [
-    "GCONF", "CHOPCONF", "GSTAT", "DRV_STATUS", "FACTORY_CONF", "IOIN",
+    "GCONF", "CHOPCONF", "DRV_STATUS", "FACTORY_CONF", "IOIN",
     "LOST_STEPS", "MSCNT", "MSCURACT", "OTP_READ", "PWM_SCALE",
     "PWM_AUTO", "TSTEP"
 ]
@@ -150,11 +149,6 @@ Fields["GCONF"] = {
     "stop_enable":              0x01 << 15,
     "direct_mode":              0x01 << 16,
     "test_mode":                0x01 << 17
-}
-Fields["GSTAT"] = {
-    "reset":                    0x01 << 0,
-    "drv_err":                  0x01 << 1,
-    "uv_cp":                    0x01 << 2
 }
 Fields["GLOBALSCALER"] = {
     "globalscaler":             0xFF << 0
